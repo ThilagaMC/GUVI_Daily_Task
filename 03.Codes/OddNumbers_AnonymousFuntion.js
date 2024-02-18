82b0674cca6dd21edd0874de167a4aa702dae731
ThilagaMC <thilaga2k2@gmail.com>
2024-02-18 11:19:26 +0530

    change

--- 03.Codes/OddNumbers_AnonymousFuntion.js
+++ 03.Codes/OddNumbers_AnonymousFuntion.js
@@ -1,9 +1,9 @@
-Nums=[1,2,3,4,5,6];
+let Nums=[1,2,3,4,5,6];
 
-for (let index = 0; index < Nums.length; index++) 
+for (let values of Nums) 
 {
-    if(Nums[index]%2!=0)
+    if(values%2!=0)
     {
-        console.log(Nums[index]);
+        console.log(values);
     }
 }
