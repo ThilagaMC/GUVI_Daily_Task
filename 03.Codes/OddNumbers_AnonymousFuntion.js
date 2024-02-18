05c5dba7ce8b5be9cbff0339306cbebd2335ec57
ThilagaMC <thilaga2k2@gmail.com>
2024-02-18 11:21:08 +0530

    changw

--- 03.Codes/OddNumbers_AnonymousFuntion.js
+++ 03.Codes/OddNumbers_AnonymousFuntion.js
@@ -1,9 +1,9 @@
 let Nums=[1,2,3,4,5,6];
 
-for (let values of Nums) 
+for (const element of Nums) 
 {
-    if(values%2!=0)
+    if(element%2!=0)
     {
-        console.log(values);
+        console.log(element);
     }
 }
