aa7c09af6bc4014db15daf1398af68ebf0a9c007
ThilagaMC <thilaga2k2@gmail.com>
2024-04-09 09:47:24 +0530

    index

---
+++ 00.Practice/Calculator.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function calculator(valueA,ValueB) 
+{
+    console.log(add(valueA,ValueB));
+    console.log(subtract(valueA,ValueB));
+    console.log(multiply(valueA,ValueB));
+    console.log(divide(valueA,ValueB));
+}
+
+calculator(9,8);
+
+
+
